8e00531f4024f056b0f25666a6231c3f7bb90ed8
xikai <1154979001@qq.com>
2020-05-12 20:54:44 +0800

    16题解题方案

--- src/main/java/JuZhengLuJing016.java
+++ src/main/java/JuZhengLuJing016.java
@@ -1,3 +1,19 @@
 public class JuZhengLuJing016 {
+    public double myPow(double x, int n) {
+        if(x == 0)return 0;
+        double res = 1.0;
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        while(n!=0){
+            if((n&1)==1){
+                res = res*x;
+            }
+            x *= x;
+            n>>>=1;
+        }
 
+        return res;
+    }
 }
