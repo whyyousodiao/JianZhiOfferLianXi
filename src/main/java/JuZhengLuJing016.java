4c4fec50c1780691f8c7f975e64db1ade62d1321
xikai <1154979001@qq.com>
2020-05-12 20:56:12 +0800

    16题解题方案

--- src/main/java/JuZhengLuJing016.java
+++ src/main/java/JuZhengLuJing016.java
@@ -1,3 +1,9 @@
+/*
+实现函数double Power(double base, int exponent)，求base的exponent次方。不得使用库函数，同时不需要考虑大数问题。
+
+        来源：力扣（LeetCode）
+        链接：https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof
+*/
 public class JuZhengLuJing016 {
     public double myPow(double x, int n) {
         if(x == 0)return 0;
