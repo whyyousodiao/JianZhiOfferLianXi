789759a48b19a3ab03283a69f312d63f236f1754
xikai <1154979001@qq.com>
2020-05-14 19:41:00 +0800

    23、24题解

---
+++ src/main/java/JuZhengLuJing023.java
@@ -0,0 +1,26 @@
+public class JuZhengLuJing023 {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = new ListNode(0);
+        ListNode rear = new ListNode(0);
+        ListNode tmp = new ListNode(0);
+        if(head == null || head.next==null){
+            return head;
+        }
+        pre = head;
+        rear = head.next;
+        while(rear != null){
+            tmp = rear.next;
+            rear.next=pre;
+            pre = rear;
+            rear = tmp;
+        }
+        head.next = null;
+        return pre;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
