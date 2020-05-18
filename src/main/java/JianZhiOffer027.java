84a7b1990cee54ed3cb5002382e7e7c064dd158d
DELL <1154979001@qq.com>
2020-05-18 09:30:55 +0800

    27题解

--- src/main/java/JianZhiOffer027.java
+++ src/main/java/JianZhiOffer027.java
@@ -1,11 +1,35 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class JianZhiOffer027 {
     public TreeNode mirrorTree(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList();
-
-        queue.add(root);
-        return null;
+        return dfs(root);
     }
+    public TreeNode dfs(TreeNode node){
+        if(node==null){
+            return null;
+        }
+        TreeNode tmpNode = new TreeNode(0);
+        tmpNode = dfs(node.left);
+        node.left = dfs(node.right);
+        node.right = tmpNode;
+        return node;
+    }
+
+    /*public TreeNode mirrorTree(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(root == null)return null;
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = new TreeNode(0);
+            node=stack.pop();
+            if(node.left != null)stack.push(node.left);
+            if(node.right != null)stack.push(node.right);
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+        }
+        return root;
+    }*/
 }
