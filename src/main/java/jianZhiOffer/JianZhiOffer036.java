34a1a2ba6bc56d08bd138802bd6a2f2313c8d33e
DELL <1154979001@qq.com>
2020-05-26 16:35:31 +0800

    '036'

--- src/main/java/jianZhiOffer/JianZhiOffer036.java
+++ src/main/java/jianZhiOffer/JianZhiOffer036.java
@@ -1,31 +1,50 @@
 package jianZhiOffer;
 
+import com.sun.xml.internal.ws.policy.sourcemodel.ModelNode;
+
 public class JianZhiOffer036 {
+
     public Node treeToDoublyList(Node root) {
-        return cur(root);
+        if(root==null)return null;
+        return cur(root).right;
     }
 
     public Node cur(Node node) {
         if(node == null)return null;
+
         Node nodeLeft = cur(node.left);
         Node nodeRight = cur(node.right);
 
         if (nodeLeft != null && nodeRight != null) {
-
-        }
-
-        if(tmpNode.right!=null){
-            nodeRight = tmpNode.right;
-        }else{
-            nodeRight = nodeLeft;
-        }
-        node.left = nodeLeft;
-        node.right= nodeRight;
-        if(node.right != null){
+            node.left = nodeLeft;
+            node.right = nodeRight.right;
+            nodeRight.right.left = node;
+            nodeRight.right = nodeLeft.right;
+            nodeLeft.right.left = nodeRight;
+            nodeLeft.right = node;
             return nodeRight;
-        }else{
+        }
+        if(nodeLeft !=null){
+            node.left = nodeLeft;
+            node.right = nodeLeft.right;
+            nodeLeft.right.left = node;
+            nodeLeft.right = node;
+
             return node;
         }
+        if(nodeRight !=null){
+            node.left = nodeRight;
+            node.right = nodeRight.right;
+            nodeRight.right.left = node;
+            nodeRight.right = node;
+            return nodeRight;
+        }
+        if(nodeRight == null && nodeLeft == null){
+            node.left = node;
+            node.right = node;
+            return node;
+        }
+        return null;
     }
 
 }
---
+++ src/main/java/jianZhiOffer/JianZhiOffer037.java
@@ -0,0 +1,17 @@
+package jianZhiOffer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JianZhiOffer037 {
+    // Encodes a tree to a single string.
+    Queue<Node> queue = new LinkedList<>();
+    public String serialize(TreeNode root) {
+        while()
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
+}
